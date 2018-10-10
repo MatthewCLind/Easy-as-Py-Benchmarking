3f028d33d510f7cbb09298bc967c3117f7519052
Matthew Lind <dnilttam@gmail.com>
2018-10-10 13:42:38 -0700

    added units and optional self-benchmarking on save

--- EZ_Benchmark.py
+++ EZ_Benchmark.py
@@ -4,6 +4,7 @@ from time import time
 start_time = -1
 last_time = 0
 results = []
+UNITS = {'day':1/(3600.0*24),'hour':1/3600.0,'minute':1/60.0,'s':1,'ms':1000,'us':1000000,'ns':1000000000} 
 
 
 def mark(label):
@@ -25,6 +26,7 @@ def add_result(label, incremental_time, total_time):
     global start_time
     global last_time
     global results
+
     
     result = {'label':label, 'increment':incremental_time, 'total time':total_time}
     results.append(result)
@@ -40,17 +42,21 @@ def clear():
     results = []
 
 
-def save_results(filename = 'results.csv'):
+def save_results(filename = 'results.csv', test_self = True, unit='s'):
+    global UNITS
     global start_time
     global last_time
     global results
+
+    factor=UNITS[unit]
     
     with open(filename, 'w') as f:
         f.write('label,increment,total time\n')
         for result in results:
-            f.write(result['label'] + ',' + str(result['increment']) + ',' + str(result['total time']) + '\n')
-        clear()
-        f.write('\n' + self_test())
+            f.write(result['label'] + ',' + str(result['increment']*factor) + ',' + str(result['total time']*factor) + '\n')
+        if test_self:
+            clear()
+            f.write('\n' + self_test())
 
 
 def self_test(n = 10000):
